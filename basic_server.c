232c4c7bff14d58e986a1220fd58895acb55f44e
Grace Chen <gchen20@stuy.edu>
2021-12-17 01:27:36 -0500

    changed to dw default code

--- basic_server.c
+++ basic_server.c
@@ -1,29 +1,10 @@
 #include "pipe_networking.h"
 
+
 int main() {
 
   int to_client;
   int from_client;
 
-  //from_client = server_handshake( &to_client );
-
-  //after client disconnect handshake starts again
-  while(1) {
-    //handshake
-    from_client = server_handshake( &to_client );
-    //continuously recieves data, process, sends back
-    while(1) {
-      //recieve data
-      char input[100];
-      int r = read(from_client, input, sizeof(input));
-      if (r == 0) break;
-
-      //process
-      input[strlen(input)-1] = '\0';
-      strcat(input, "manipulated");
-
-      //sends back
-      write(to_client, input, sizeof(input));
-    }
-  }
+  from_client = server_handshake( &to_client );
 }
